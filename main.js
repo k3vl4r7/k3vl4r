bc4e574835028f5c66854e8b50eb979ea7a998bd
Kev Lar <k3vl4r@Kevs-MacBook-Pro.local>
2024-06-10 18:39:18 -0400

    Adding express server

--- main.js
+++ main.js
@@ -30,7 +30,14 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  const app = express();
+  const port = 3000;
+
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    createWindow()
+  });
+
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
